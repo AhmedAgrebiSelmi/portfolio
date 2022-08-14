a3e7937f4b9827cd4ba8e3fc4059db9004cb2ce9
unknown <Ahmed.agrebiselmi@gmail.com>
2022-08-14 10:29:25 +0200

    set up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Navbar from"./components/navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Hello</h1>
+      <button className="btn btn-success">OK</button>
     </div>
   );
 }
