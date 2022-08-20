9ea3ef8045e2ad297a93a4db7fe822d77556967d
Ahmed Agrebi Selmi <Ahmed.agrebiselmi@gmail.com>
2022-08-20 12:57:44 +0200

    Add not found page

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Projects from './pages/Projects';
 import Skills from './pages/Skills';
 import Home from './pages/Home';
 import SkillDetails from './pages/SkillDetails';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
 
       <BrowserRouter>
         <Navbar />
-        <div className='container'>
+        <div className='container mt-5'>
           <Switch>
         <Route path='/contact' component={Contact}  />
         <Route path='/projects' component={Projects}  />
         <Route path='/skills/:id' component={SkillDetails}/>
         <Route path='/skills' component={Skills}  />
         <Route exact path='/' component={Home}  />
+        <Route component={NotFound}  />
         </Switch>
         </div>
       </BrowserRouter>
