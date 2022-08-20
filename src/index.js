c47ec2ece8f43073673d44c88c48375b0083589d
Ahmed Agrebi Selmi <Ahmed.agrebiselmi@gmail.com>
2022-08-20 11:06:35 +0200

    add contenr to the contact page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 //C:\Users\ASUS\Projects\portfolio\node_modules\bootstrap\dist\css\bootstrap.min.css
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 import './index.css';
 import App from './App';
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 function Contact() {
+  const ContactInformations = {
+    email: 'ahmed.agrebiselmi@gmail.com',
+    phoneNumber: '+216 24 585 757',
+    adress: {
+    city: 'Sfax',
+    Country: 'Tunisia',
+    Street: 'AGAREB CITY',
+    postalCode: 3030
+  }
+
+  }
   return (
-    <div>Contact</div>
+    <div>
+      <p> <i class="bi bi-envelope"></i> {ContactInformations.email}</p>
+      <p> <i class="bi bi-telephone"></i> {ContactInformations.phoneNumber}</p>
+      <p> <i class="bi bi-geo-alt"></i> {ContactInformations.adress.Street}; {ContactInformations.adress.postalCode} {ContactInformations.adress.city}; {ContactInformations.adress.Countrynp}</p>
+    </div>
   )
 }
 
